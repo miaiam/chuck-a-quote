c424721fc9cc15cdec7454151196f0cbab9a3c1c
Mia Wölm <mia.woelm@gmail.com>
2023-12-12 10:52:17 +0100

    add image to main section

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <div className='flex max-h-screen flex-col'>
+          <Navbar />
+          {children}
+        </div>
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main>
-      
+    <main className="flex relative h-screen">
+      <Image
+        alt="Galaxy and landscape in neon purple and blue colors"
+        src="/assets/galaxy.png"
+        layout="fill"
+        objectFit="cover"
+      />
     </main>
   )
 }
