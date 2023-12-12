13d1a3453442588c6eb21e83f9240f9ab751b9e5
Mia Wölm <mia.woelm@gmail.com>
2023-12-12 11:21:21 +0100

    add button component

--- app/components/quote/index.tsx
+++ app/components/quote/index.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import { useEffect, useState } from "react";
+import Button from "../button";
 
 const CATEGORY = "dev";
 const API_ROUTE = `https://api.chucknorris.io/jokes/random?category=${CATEGORY}`;
 
 const Quote = () => {
-  const [quote, setQuote] = useState(null);
+  const [quote, setQuote] = useState<string|null>(null);
 
   useEffect(() => {
     getQuote();
@@ -17,9 +18,13 @@ const Quote = () => {
     const data = await res.json();
     setQuote(data?.value);
   }
-  
 
-  return (<div className="text-4xl font-bold">{quote}</div>);
+  return (
+    <div className="flex flex-col items-center gap-4 md:justify-center md:gap-6">
+      <div className="md:text-4xl text-2xl font-bold text-center">{quote}</div>
+      <Button onClick={getQuote} value="Change quote" />
+    </div>
+  );
 }
 
 export default Quote;
--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-dark-bg text-white`}>
         <div className='flex max-h-screen flex-col p-4'>
           <Navbar />
           {children}
