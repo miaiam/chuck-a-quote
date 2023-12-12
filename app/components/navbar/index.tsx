6aa36b6722265ade8bea9a7b5b325fa7d90d27a2
Mia Wölm <mia.woelm@gmail.com>
2023-12-12 10:39:43 +0100

    add navbar with w&s logo

---
+++ app/components/navbar/index.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+const Navbar = () =>
+  <div className="w-full p-4">
+    <Image alt="W&S Logo" src="assets/w&s-logo.svg" width={99.7} height={64.9} />
+  </div>
+
+export default Navbar;
